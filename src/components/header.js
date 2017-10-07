141d2b98c84c23438fdc35db10cbe9c4db6c3442
sochisic <trexx371@gmail.com>
2017-10-07 14:47:31 +0300

    Changes in Header and added header menu button

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import iphone from '../img/iphone.svg';
-import styles from '../styles/header.css';
+import '../styles/header.css'
+import HeaderButton from './headerbutton';
 
 class Header extends Component {
 
   render() {
     return (
-      <div className="App--Header Prose-splash">
+      <div className="App--Header">
         <img src={iphone} className="Header--logo" alt="logo" />
         <h2 className="Header--text">Привет! Я телефонный мастер Вася!</h2>
+        <HeaderButton />
+
       </div>
     );
   }
---
+++ src/components/headerbutton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import IconButton from 'material-ui/IconButton';
+import Menu from '../../node_modules/material-ui/svg-icons/navigation/menu';
+
+const styles = {
+  smallIcon: {
+    width: 36,
+    height: 36,
+  },
+  mediumIcon: {
+    width: 48,
+    height: 48,
+  },
+  largeIcon: {
+    width: 60,
+    height: 60,
+  },
+  small: {
+    width: 72,
+    height: 72,
+    padding: 16,
+  },
+  medium: {
+    width: 96,
+    height: 96,
+    padding: 24,
+  },
+  large: {
+    width: 120,
+    height: 120,
+    padding: 30,
+  },
+};
+
+const HeaderButton = () => (
+  <div className="Header--button">
+    <IconButton
+      iconStyle={styles.mediumIcon}
+      style={styles.medium}
+    >
+      <Menu />
+    </IconButton>
+  </div>
+);
+
+export default HeaderButton;
